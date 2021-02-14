d0dcc5c4d305a7cfffe5d84b1b6b442edda19228
josh5734 <61929745+josh5734@users.noreply.github.com>
2021-02-14 23:24:16 +0900

    Update Programmers_타겟 넘버.py

--- Programmers_타겟 넘버.py
+++ Programmers_타겟 넘버.py
@@ -1,11 +1,28 @@
-def solution(numbers, target):
-    answer_list = [0]
-    for i in numbers:
-        sum_list = []
-        for j in answer_list:
-            sum_list += [j + i]
-            sum_list += [j - i]
-        answer_list = sum_list
+from collections import deque
 
-    answer = answer_list.count(target)
-    return answer
+
+def solution(numbers, target):
+    q = deque()
+    # 처음에 0을 넣고 시작
+    q.append(0)
+    # 주어진 numbers를 앞에서부터 순회
+    while numbers:
+        # 현재 단계의 값들에 대해 +, -를 해서 나올 수 있는 경우를 temp에 저장
+        temp = []
+        while q:
+            now = q.popleft()
+            temp.append(now + numbers[0])
+            temp.append(now - numbers[0])
+        numbers.pop(0)
+        # temp를 q에 저장
+        q.extend(temp)
+
+    # 결과적으로 q에는 numbers를 서치하면서 +, -한 최종 결과가 저장됌
+    return q.count(target)
+
+
+if __name__ == "__main__":
+    numbers = [1, 1, 1, 1, 1]
+    target = 3
+    answer = solution(numbers, target)
+    print(answer)
