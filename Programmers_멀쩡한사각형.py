a5916762dc8f7344d725e7e539ce5d14696d628c
josh5734 <61929745+josh5734@users.noreply.github.com>
2021-02-24 15:03:53 +0900

    Update Programmers_멀쩡한사각형.py

--- Programmers_멀쩡한사각형.py
+++ Programmers_멀쩡한사각형.py
@@ -1,3 +1,23 @@
+import math
+
+# 정답 풀이 - 최대 공약수 활용
+# 우선 w와 h가 공약수가 있다면 문제를 공약수를 나눈 w' 와 h'로 축소시킬 수 있다.
+# w'와 h'가 서로소라 가정했을 때, 대각선은 반대쪽 코너에 도달하기전 w'-1 세로선과 h'-1 가로선을 지나고 지날때마다 새로운 정사각형이 추가된다.
+# 그래서 첫 정사각형을 포함 1 + (w'-1) + (h'-1) = w' + h' - 1개의 정사각형을 지나게 되므로
+# 공약수를 다시 곱해주면 w + h - gcd(w,h)개의 정사각형을 지나는것을 찾을 수 있다.
+
+
+def solution(w, h):
+    return w*h - w - h + math.gcd(w, h)
+
+
+if __name__ == "__main__":
+    w, h = 8, 12
+    print(solution(w, h))
+
+
+'''
+########### 테스트케이스 13, 16 시간 초과 #############
 def solution(w, h):
     if w == 1 or h == 1:
         return 0
@@ -12,13 +32,9 @@ def solution(w, h):
     for i in range(1, h):
         start, end = dx * i, dx * (i+1)
         # 끝부분이 모서리에 닿지 않고, 격자선이 한 변을 뚫고 지나갈 때는 두개가 날라감
-        if end - int(end) != 0 and (int(end) - int(start) == 1):
+        if i*dx - int(i*dx) != 0 and math.ceil(i * dx) != math.ceil((i+1)*dx):
             answer -= 2
         else:
             answer -= 1
     return answer
-
-
-if __name__ == "__main__":
-    w, h = 8, 12
-    print(solution(w, h))
+'''
