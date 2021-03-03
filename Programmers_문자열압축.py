b1cdb908c0c1c1aa928a8db9c5aa202d6b1f03ad
josh5734 <61929745+josh5734@users.noreply.github.com>
2021-03-03 21:11:26 +0900

    Update Programmers_문자열압축.py

--- Programmers_문자열압축.py
+++ Programmers_문자열압축.py
@@ -1,30 +1,30 @@
 def solution(s):
     if len(s) == 1:
         return 1
-    stringCompressionLength = []  # 압축된 문자열의 길이를 담는 리스트
+    shortestLength = len(s)
     # 문자열 압축을 1~(문자열 길이의 절반)까지 해본다.
     for l in range(1, (len(s)//2+1)):
         compressedStack = []
         cursor = 0
-        count = 1
         while cursor < len(s):
             nextString = s[cursor:cursor+l]
-            if len(compressedStack) == 0:  # 스택에 아무것도 없으면 그냥 삽입
-                compressedStack.append(count)
+            # 스택에 아무것도 없거나, 이전 문자와 현재 문자가 다르면 1과 문자열을 삽입한다.
+            if len(compressedStack) == 0 or compressedStack[-1] != nextString:
+                compressedStack.append(1)
                 compressedStack.append(nextString)
-            elif compressedStack[-1] == nextString:  # 이전 문자와 현재 문자가 같다면 반복
-                compressedStack[-2] += 1  # 반복되는 카운트를 1 증가시킨다.
-            else:
-                compressedStack.append(count)  # 이전 문자와 현재 문자가 다르면 그냥 삽입
-                compressedStack.append(nextString)
+            # 이전 문자와 현재 문자가 같다면 반복되는 카운트를 1 증가시킨다.
+            elif compressedStack[-1] == nextString:
+                compressedStack[-2] += 1
             cursor += l  # 반복되는 문자열의 길이 l만큼 커서 이동
         # 스택에서 1 제거
         compressedStack = map(
             str, list(filter(lambda x: x != 1, compressedStack)))
         compressedString = ''.join(compressedStack)
-        stringCompressionLength.append(len(compressedString))
 
-    return min(stringCompressionLength)
+        if len(compressedString) < shortestLength:
+            shortestLength = len(compressedString)
+
+    return shortestLength
 
 
 if __name__ == "__main__":
