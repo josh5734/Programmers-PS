87d659b6b1b952d87f047131f325ef51d651db43
josh5734 <61929745+josh5734@users.noreply.github.com>
2021-03-15 11:48:40 +0900

    Update Programmers_프린터.py

--- Programmers_프린터.py
+++ Programmers_프린터.py
@@ -1,18 +1,27 @@
-def solution(priorities, location):
-    answer = 0
+from collections import deque
 
-    while len(priorities) != 0:
-        if priorities[0] == max(priorities):
-            answer += 1
-            priorities.pop(0)
-            if location == 0:
-                return answer
-            else:
-                location -= 1
+
+def solution(priorities, location):
+    printer = deque()
+    for idx, priority in enumerate(priorities):
+        printer.append((idx, priority))
+
+    priorities.sort(reverse=True)  # 우선순위가 높은 순서대로 정렬
+    max_idx = 0
+
+    count = 0
+    while True:
+        idx, p = printer.popleft()
+        if p == priorities[max_idx]:  # p가 우선순위가 가장 높다면 제거
+            count += 1
+            max_idx += 1  # 기존 priorities 배열에서 idx를 +1 해서 다음 최고 우선순위값 서치
+            if idx == location:
+                return count
         else:
-            priorities.append(priorities.pop(0))
-            if location == 0:
-                location = len(priorities) - 1
-            else:
-                location -= 1
-    return answer
+            printer.append((idx, p))
+
+
+if __name__ == "__main__":
+    priorities = [1, 1, 9, 1, 1, 1]
+    location = 0
+    print(solution(priorities, location))
